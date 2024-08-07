2ee0d6f6a9076ac8dd05b646d580fd3b0c0c3232
Leticia Cristina <letici4cr1s@gmail.com>
2024-08-07 17:04:03 -0300

    .

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById('search-button').addEventListener('click', function() {
+    const query = document.getElementById('search-bar').value;
+    alert('You searched for: ' + query);
+    // Aqui você pode adicionar a lógica de pesquisa
+});

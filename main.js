d23c04bef9e1a9c9eae1c3f2aff6281532f14075
Leticia Cristina <letici4cr1s@gmail.com>
2024-08-09 03:58:52 -0300

    ok

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-document.getElementById('search-button').addEventListener('click', function() {
+document.getElementById('pesquisar').addEventListener('click', function() {
     const query = document.getElementById('search-bar').value;
-    alert('You searched for: ' + query);
+    alert('Você procurou por: ' + query);
     // Aqui você pode adicionar a lógica de pesquisa
 });
+
+document.querySelector('.dropdown').addEventListener('click', function() {
+    this.querySelector('.box-dropdown').classList.toggle('show');
+});
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown')) {
+        var dropdowns = document.querySelectorAll('.box-dropdown');
+        dropdowns.forEach(function(dropdown) {
+            if (dropdown.classList.contains('show')) {
+                dropdown.classList.remove('show');
+            }
+        });
+    }
+};
